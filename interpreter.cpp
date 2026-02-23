376c266a0abaa31666ab06ca710286a149d08aaf
pseudoclone <prince.077bei027@acem.edu.np>
2026-02-24 03:35:16 +0545

    feat(INTERPRETER): Read contents of file I still haven't figured out how to use mmap and lseek

--- interpreter.cpp
+++ interpreter.cpp
@@ -3,6 +3,7 @@
 #include<cstdint>
 #include<string>
 #include<fstream>
+#include<algorithm>
 
 class BrainFuck{
         private:
@@ -12,10 +13,13 @@ class BrainFuck{
                 BrainFuck(){
                         buffer.push_back(0);
                 }
-                void moveRight();
-                void moveLeft();
-                std::uint8_t read() const;
+                void moveRight(void);
+                void moveLeft(void);
+                std::uint8_t read(void) const;
                 void write(uint8_t value);
+                void increment(void);
+                void decrement(void);
+                void loop(void);
 };
 
 void BrainFuck::moveRight(void){
@@ -27,7 +31,7 @@ void BrainFuck::moveRight(void){
 
 void BrainFuck::moveLeft(void){
         if(pointerHead == 0){
-                throw std::out_of_range("BrainFuck Error: Trying to move pointer head to negative indices");
+                throw std::out_of_range("BrainFuck Error: Trying to move pointer head to negative index");
         }
         pointerHead--;
 }
@@ -40,15 +44,65 @@ void BrainFuck::write(std::uint8_t value){
         buffer[pointerHead] = value;
 }
 
-void jump(){}
+void BrainFuck::increment(){
+        buffer[pointerHead]++;
+}
+
+void BrainFuck::decrement(){
+        buffer[pointerHead]--;
+}
+
+void BrainFuck::loop(){
+
+}
 
 
 class Interpreter{
+        private:
+                std::ifstream file{};
         public:
-                Interpreter(const std::string& filename){
+                BrainFuck bf{};
+                Interpreter(const std::string& filename): file{filename}{
                         std::ifstream file{filename};
                         if(!file.is_open()){
                                 throw std::runtime_error("File Error: Error opening file");
                         }
                 }
+        void eatChars(void);
+        void loop
 };
+
+void Interpreter::eatChars(void){
+        std::istreambuf_iterator<char> begin{file};
+        std::istreambuf_iterator<char> end{};
+
+        std::for_each(begin, end, [&](char c){
+                switch (c)
+                {
+                        case '>':
+                                bf.moveRight();
+                                break;
+                        case '<':
+                                bf.moveLeft();
+                                break;
+                        case '+':
+                                bf.increment();
+                                break;
+                        case '-':
+                                bf.decrement();
+                                break;
+                        case '.':
+                                bf.write(); /* Write to output*/
+                                break;
+                        case ',':
+                                std::cout << (char)bf.read(); /* Read from input */
+                                break;
+                        default:
+                                break;
+                }
+        });
+}
+
+int main(int argc, char **argv){
+        //
+}
